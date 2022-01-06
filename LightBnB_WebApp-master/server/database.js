1a6009c57c96461aab58ebd3f2a57f1086b0fb1e
Emily-Waters <emilyabigailwaters@gmail.com>
2022-01-06 19:07:40 +0000

    implemented getAllReservations in database.js using pg

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -60,7 +60,11 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT * FROM properties JOIN reservations ON properties.id = reservations.property_id WHERE reservations.guest_id = $1 LIMIT $2`,
+      [guest_id, limit])
+    .then(res => res.rows)
+    .catch(err => err);
 };
 exports.getAllReservations = getAllReservations;
 
